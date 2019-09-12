ffbbfe9a0dec966788c8eb0e4f2c380e84e50e13
Wesley oliveira <wesleyumsocaminho@gmail.com>
2019-09-12 13:39:23 -0300

    I created func db_select to read mynews.db

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import News
+import sqlite3
+   
 
-# Create your views here.
+
+def db_select():
+    path = '/scraperNews/spiders/mynews.db'
+    con = sqlite3.connect(path)
+    cur = con.cursor()
+    sql = """
+    SELECT Title
+    FROM news
+    """
+    cur.execute(sql)
+    data = cur.fetchall()
+    con.close()
+    return data
